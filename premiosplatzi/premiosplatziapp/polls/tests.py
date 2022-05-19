85791e2a2a05da8c9b26103605197daa79af5a0a
Chauxxx06 <freemanchx@hotmail.com>
2022-05-18 17:47:51 -0700

    Ajustando CSS al aplicativo y normalizando HTML

--- premiosplatzi/premiosplatziapp/polls/tests.py
+++ premiosplatzi/premiosplatziapp/polls/tests.py
@@ -40,9 +40,9 @@ class QuestionIndexViewTest(TestCase):
     
     def test_no_questions(self):
         """ If no question exist, an appropiate message is displayed"""
-        response = self.client.get(reverse("polls:index"))
-        self.assertEqual(response.status_code,200)
-        self.assertContains(response, "No Polls are available.")
+        response = self.client.get(reverse("polls:index")) #reverse hace que no se harcodee la url (es dinamico) y self.client.get es la peticion http GET
+        self.assertEqual(response.status_code,200) # response.status_code retorna estado respuesta http
+        self.assertContains(response, "No Polls are available.") # el mensaje viene el frontend
         self.assertQuerysetEqual(response.context["latest_question_list"],[])
 
     def test_future_question(self):
@@ -61,4 +61,58 @@ class QuestionIndexViewTest(TestCase):
         question = create_question("past_question", days=-30)
         response = self.client.get(reverse("polls:index"))
         self.assertQuerysetEqual(response.context["latest_question_list"],[question])
-    
+
+    def test_future_question_and_past_question(self):
+        """
+        Even if both past and future question exist, only past question are display
+        """
+        past_question = create_question(question_text="past_question", days=-30)
+        future_question = create_question(question_text="future_question", days=30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerysetEqual(
+            response.context["latest_question_list"],
+            [past_question]
+        )
+
+    def test_two_past_question(self):
+        """
+        The question index page may display multiple question
+        """
+        past_question1 = create_question(question_text="past_question_1", days=-20)
+        past_question2 = create_question(question_text="past_question_2", days=-30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerysetEqual(
+            response.context["latest_question_list"],
+            [past_question1, past_question2]
+        )
+
+    def test_two_future_question(self):
+        """
+        The question index page may display multiple question
+        """
+        future_question1 = create_question(question_text="future_question_1", days=20)
+        future_question2 = create_question(question_text="future_question_2", days=30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertContains(response, "No Polls are available.")
+        self.assertQuerysetEqual(response.context["latest_question_list"],[])
+
+class QuestionDetailViewTest(TestCase):
+    def test_future_question(self):
+        """
+        Teh detail view of a question with a pub_date in the future
+        return a 404 error not found
+        """
+        future_question = create_question(question_text="future_question", days=30)
+        url = reverse("polls:detail",args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """
+        the detail view of a question with a pub_date in the past
+        display dthe question text
+        """
+        past_question = create_question(question_text="past_question", days=-30)
+        url = reverse("polls:detail",args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
